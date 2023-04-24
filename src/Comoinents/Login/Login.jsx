f0bab7a242265036495615429eeb60336c851d16
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-04-25 00:24:32 +0600

    final added

--- src/Comoinents/Login/Login.jsx
+++ src/Comoinents/Login/Login.jsx
@@ -1,12 +1,17 @@
 
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import app from '../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
 
+const auth = getAuth(app);
 
 const Login = () => {
     const [error,setError] = useState('');
     const [success ,setSuccess] = useState('');
+    const emailRef = useRef();
 
 
     const handleLogin = event => {
@@ -34,7 +39,43 @@ const Login = () => {
          else if(password.length <6){
             setError('password is too short and it must be 6 characters long')
          }
+
+         signInWithEmailAndPassword(auth,email,password)
+         .then(result =>{
+            const loggedUser = result.user;
+            
+            console.log(loggedUser);
+            if(!loggedUser.emailVerified){
+
+            }
+
+
+            setSuccess('user log in successful')
+            setError('')
+
+
+         } )
+         .catch(error => {
+            setError(error.message);
+         })
     }
+
+ const handleResetPassword = event => {
+    const email = emailRef.current.value ;
+    if(!email){
+        alert('please provide your email address to reset passwprd ')
+        return;
+    }
+    sendPasswordResetEmail(auth,email)
+    .then( () => {
+        alert('Please check your email')
+    })
+    .catch(error => {
+        console.log(error);
+        setError(error.message)
+    })
+ }
+
     return (
         <div  className=' w-25 mx-auto '>
 
@@ -44,7 +85,7 @@ const Login = () => {
       <form onSubmit={handleLogin}>
         <div className="form-group mb-3">
           <label htmlFor="emailInput">Email address</label>
-          <input type="email"  name="email" className="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email" required/>
+          <input type="email"  name="email" ref={emailRef} className="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email" required/>
          
         </div>
 
@@ -60,7 +101,8 @@ const Login = () => {
 
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
-
+      <p><small>Forget password ? Please <button onClick={handleResetPassword} className='btn btn-link'>Reset Password</button></small></p>
+            <p><small>New to this website ? please log in<Link to="/register">Register</Link> </small></p>
             <p className='text-danger'>{error}</p>
             <p className='text-success'>{success}</p>
 
--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile} from 'firebase/auth'
 import app from '../firebase/firebase.config';
+import Login from '../Comoinents/Login/Login';
+import { Link } from 'react-router-dom';
 
 
 const auth = getAuth(app)
@@ -20,7 +22,8 @@ const Register = () => {
                 // 2.collect from data
                 const email = event.target.email.value;
                 const password = event.target.password.value;
-                    console.log(email ,password);
+                const name = event.target.name.value;
+                    console.log(name ,email ,password);
                     // validate
                     if(!/(?=.*[A-Z])/.test(password)){
                         setError('Please add least one uppercase');
@@ -42,7 +45,9 @@ const Register = () => {
                     console.log(loggedUser);
                     setError('')
                     event.target.reset();
-                    setSuccess('user has successfully created')
+                    setSuccess('user has successfully created');
+                    sendVerificationEmail(result.user)
+                    updateUserData(result.user, name)
                 })
                 .catch(error => {
                     console.error(error.message);
@@ -52,7 +57,27 @@ const Register = () => {
                 })
             }
 
+            const sendVerificationEmail = (user) => {
+              sendEmailVerification(user)
+              .then(result => {
+                console.log(result);
+                alert('please verify your email')
+              })
+            }
 
+            const updateUserData = ( user,name) =>{
+                updateProfile(user, {
+                    displayName :name
+                })
+                .then(() => {
+                    console.log('user name updated ');
+                })
+                .catch(error =>{
+                    console.log(error);
+                    setError(error.message)
+                })
+
+            }
 
         const handleEmailChange = (event) => {
             console.log(event.target.value);
@@ -67,12 +92,16 @@ const Register = () => {
         <div className='w-50 mx-auto'>
             <h4> Please Register</h4>
             <form onSubmit={handleSubmit}>
+                <input className='w-50 mb-4 rounded ps-2 ' type="text" name="name" id="name" placeholder="Your name address"  required/>
+                <br></br>
                 <input className='w-50 mb-4 rounded ps-2 ' onChange={handleEmailChange} type="email" name="email" id="email" placeholder="Your email address"  required/>
                 <br></br>
                 <input className='w-50 mb-4 rounded ps-2 '  onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder="Your password" required />
                 <br />
                 <input className='btn btn-primary rounded'  type="submit" value="Register" />
             </form>
+
+            <p><small>Already have an account ? please <Link to="/login">Log In</Link></small></p>
             
             <p className='text-danger'>{error}</p>
             
